d33b1a2a321c37ddae4fcc80995ac8cdece11683
Minjeong Kim <101111603+Mingguriguri@users.noreply.github.com>
2024-08-01 15:50:10 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "minggirokkk",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/minjeong-avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "backend developer",
     bio: "Always learning new things",
     email: "minbory925@gmail.com",
